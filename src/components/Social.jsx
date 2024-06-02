e930f291b6f3437f88ed045a5f90fe65d6b7fd77
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-06-02 22:45:46 +0700

    Minor Update

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 import { FaGithub, FaLinkedin, FaInstagram, FaXTwitter } from "react-icons/fa6"
 
 const socials = [
-    { icon: <FaGithub />, path: ""},
-    { icon: <FaLinkedin />, path: ""},
-    { icon: <FaInstagram />, path: ""},
+    { icon: <FaGithub />, path: "https://github.com/MahardhikaB"},
+    { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/mahardhika-bredy-39628a1a2/"},
+    { icon: <FaInstagram />, path: "https://www.instagram.com/mhrdhka.bd/"},
     { icon: <FaXTwitter />, path: ""},
 ]
 
@@ -14,7 +14,7 @@ const Social = ({ containerStyles, iconStyles }) => {
         <div className={containerStyles}>
             {socials.map((item, index) => {
                 return (
-                    <Link key={index} href={item.path} className={iconStyles}>
+                    <Link key={index} href={item.path} className={iconStyles} target="_blank">
                         {item.icon}
                     </Link>
                 )
